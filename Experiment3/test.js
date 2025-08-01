048a967f1fc156b8c2c952c5979a6d75c20b07e8
Vik3sh <negivikesh2@gmail.com>
2025-08-01 18:52:49 +0530

    practiced the experiment 3rd

---
+++ Experiment3/test.js
@@ -0,0 +1,6 @@
+
+const checkbox = document.querySelector("input[type=checkbox]");
+const html = document.querySelector("html");
+checkbox.addEventListener("change", () => {
+  html.classList.toggle("dark");
+});
